b37173c45dd98d723128a08aa57e943414443536
AnaFidencio <fidenciosilvaanaclara@gmail.com>
2024-04-08 14:52:59 -0300

    Alteração no arquivo descrição

--- core/models/acessorio.py
+++ core/models/acessorio.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Acessorio(models.Model):
-    descrição = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100)
 
     def __str__(self):
-        return f'{self.descrição} ({self.id})'
+        return f'{self.descricao} ({self.id})'
